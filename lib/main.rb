a468f23a399107be468c3960804015492b9b5403
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2023-02-10 10:35:42 -0500

    refactor classes

--- lib/main.rb
+++ lib/main.rb
@@ -1,17 +1,20 @@
 require './lib/most_common_word_sequences_of_file'
 
-class CommonWordSequencesMultiFiles
+class Main
   def call
     ARGV.each { |file_name|
       begin
+        return "Please only enter .txt files" unless file_name.end_with?('.txt')
         file = File.open(file_name)
-        MostCommonWordSequencesOfFile.new(file).call
+        result = MostCommonWordSequencesOfFile.new(file).call
         file.close()
-      rescue
-        print "#{file_name} does not exist. proceeding..."
+      rescue NoMethodError
+        raise
+      # rescue
+      #   print "#{file_name} does not exist. proceeding..."
       end
     }
   end
 end
 
-print CommonWordSequencesMultiFiles.new.call
+Main.new.call
--- lib/most_common_word_sequences_of_file.rb
+++ lib/most_common_word_sequences_of_file.rb
@@ -1,53 +1,50 @@
 class MostCommonWordSequencesOfFile
+  attr_writer :result
   NUM_OF_WORDS_IN_SEQUENCE = 3
   NUM_OF_SEQUENCES = 100
 
   def initialize(file)
     @file = file
+    @result = nil
   end
 
   def call
-    freq = word_sequence_frequencies(strip)
-    hash = descend(freq)
-    top_common_word_sequences(hash)
+    strip
+    word_sequence_frequencies
+    descend
+    top_common_word_sequences
+    print_top_word_sequences
+  end
+
+  def print_top_word_sequences
+    print @result.keys
   end
 
   def strip
     file_data = @file.read.downcase.split()
-    file_data.map!{|data| data.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '')}
+    @result = file_data.map!{|data| data.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '')}.reject(&:empty?)
   end
 
-  def sequence_has_empty_strings(data, index)
-    data[index..index + NUM_OF_WORDS_IN_SEQUENCE - 1].any?(&:empty?)
+  def word_seq(index)
+    @result[index..index + NUM_OF_WORDS_IN_SEQUENCE - 1].join(' ')
   end
 
-  def word_seq(data, index)
-    data[index..index + NUM_OF_WORDS_IN_SEQUENCE - 1].join(' ')
-  end
+  def word_sequence_frequencies
+    frequency_dict = Hash.new(0)
 
-  def word_sequence_frequencies(file_data)
-    frequency_dict = Hash.new
-
-    for i in 0..file_data.length-NUM_OF_WORDS_IN_SEQUENCE do
-      next if sequence_has_empty_strings(file_data, i)
-
-      word_sequence = word_seq(file_data, i)
-
-      if frequency_dict.has_key?(word_sequence)
-        frequency_dict[word_sequence] += 1
-      else
-        frequency_dict[word_sequence] ||= 0
-      end
+    for i in 0..@result.length-NUM_OF_WORDS_IN_SEQUENCE do
+      word_sequence = word_seq(i)
+      frequency_dict[word_sequence] += 1
     end
 
-    frequency_dict
+    @result = frequency_dict
   end
 
-  def descend(hash)
-    hash.sort_by {|k, v| -v}
+  def descend
+    @result = Hash[@result.sort_by{|k,v| -v}]
   end
 
-  def top_common_word_sequences(hash)
-    hash.take(NUM_OF_SEQUENCES)
+  def top_common_word_sequences
+    @result = Hash[@result.take(NUM_OF_SEQUENCES)]
   end
 end
