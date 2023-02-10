ac898428dcb4a5e85c94f36bd6787f6b387daa8d
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2023-02-10 15:24:25 -0500

    shorten tests

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,15 +1,34 @@
 require 'main'
 
-describe 'Main' do
-  describe "call" do
-    context "all files by user exist" do
-      it "calls Main for all files" do
+describe Main do
+  let!(:file_names) {[]}
 
+  describe "call" do
+    subject {
+      described_class.new(file_names).call
+    }
+
+    context "the user does not list files" do
+      it "return a help command" do
+        expect(subject).to eq([])
       end
     end
-    context "all files by user exist" do
-      it "returns an error message but continues the loop" do
-
+    context "one of the files is not a txt file" do
+      let!(:file_names) {['spec/empty.txt', 'spec/full_of_.txt']}
+      it "emits a msg but continues the program despite the invalid txt file" do
+        expect(subject).to eq(['spec/empty.txt', 'spec/full_of_.txt'])
+      end
+    end
+    context "one of the files does not exist" do
+      let!(:file_names) {['spec/empty.jpeg', 'spec/full_of_.txt']}
+      it "continues the program despite the invalid txt file" do
+        expect(subject).to eq("spec/empty.jpeg is not a .txt file. Please only enter .txt files.")
+      end
+    end
+    context "all the txt files are valid" do
+      let!(:file_names) {['spec/empty.txt', 'spec/full_of_.txt']}
+      it "prints the MostCommonWordSequencesOfFile of all files" do
+        expect(subject).to eq(['spec/empty.txt', 'spec/full_of_.txt'])
       end
     end
   end
--- spec/most_common_word_sequences_of_file_spec.rb
+++ spec/most_common_word_sequences_of_file_spec.rb
@@ -1,93 +1,24 @@
 require 'most_common_word_sequences_of_file'
 
-describe 'MostCommonWordSequencesOfFile' do
+describe MostCommonWordSequencesOfFile do
+  let!(:file_name) { "spec/empty.txt" }
+  let(:file) { File.open(file_name) }
+
   describe "call" do
-    it "returns hash which meets challenge criteria" do
-
-    end
-  end
-  describe "strip" do
-    it "strips the data" do
-
-    end
-  end
-  describe "sequence_has_empty_strings" do
-    let!(:data) {}
-    let!(:index) {}
-    let(:sequence_has_empty_strings) { MostCommonWordSequencesOfFile.new('blah.jpg').sequence_has_empty_strings(data, index) }
-    context "has empty strings starting at index 0" do
-      let!(:data) { ['', 'b', 'e', 'g'] }
-      let!(:index) { 0 }
-      it "returns true" do
-        expect(sequence_has_empty_strings).to eq(true)
+    subject {
+      described_class.new(file).call
+    }
+    context "txt file is empty" do
+      it "returns an empty hash" do
+        expect(subject).to eq({})
       end
     end
-    context "has empty strings starting at index 1" do
-      let!(:data) { ['', 'b', 'e', 'g'] }
-      let!(:index) { 1 }
-      it "returns false" do
-        expect(sequence_has_empty_strings).to eq(false)
+    context "txt file has data" do
+      let!(:file_name) { "spec/full_of_.txt" }
+      it "returns a string" do
+        expect(subject).to be_a_kind_of(Hash)
+        expect(subject.length).to be <= MostCommonWordSequencesOfFile::NUM_OF_SEQUENCES
       end
-    end
-    context "does not have empty strings" do
-      let!(:data) { ['pl', 'b', 'e', 'g', 'e'] }
-      let!(:index) { 2 }
-      it "returns false" do
-        expect(sequence_has_empty_strings).to eq(false)
-      end
-    end
-  end
-  describe "word_seq" do
-    let!(:data) {}
-    let!(:index) {}
-    let(:word_seq) { MostCommonWordSequencesOfFile.new('blah.jpg').word_seq(data, index) }
-    context "array has only empty strings" do
-      let!(:data) { ['', '', ''] }
-      let!(:index) { 0 }
-      it "returns empty string" do
-        expect(word_seq).to eq("  ")
-      end
-    end
-    context "array has one empty string" do
-      let!(:data) { ['', 'a', 'b'] }
-      let!(:index) { 0 }
-      it "returns string with all non-empty strings" do
-        expect(word_seq).to eq(" a b")
-      end
-    end
-    context "array of has non-empty strings" do
-      let!(:data) { ['a', 'b', 'd', 'e'] }
-      let!(:index) { 1 }
-      it "returns string with #{MostCommonWordSequencesOfFile::NUM_OF_WORDS_IN_SEQUENCE} strings starting at index 1}" do
-        expect(word_seq).to eq("b d e")
-      end
-    end
-    context "index is near end of array" do
-      let!(:data) { ['a', 'b', 'd', 'e'] }
-      let!(:index) { 3 }
-      it "returns string with #{MostCommonWordSequencesOfFile::NUM_OF_WORDS_IN_SEQUENCE} strings starting at index 3" do
-        expect(word_seq).to eq("e")
-      end
-    end
-
-  end
-  describe "word_sequence_frequencies" do
-    it "returns the correct frequencies based on the input" do
-
-    end
-  end
-  describe "descend" do
-    let(:descent) { MostCommonWordSequencesOfFile.new('blah.jpg').descend({:h => 3, :p => 10, :k => 1, :q => 6}) }
-    it "descends the hash by value" do
-      expect(descent).to eq([[:p, 10], [:q, 6], [:h, 3], [:k, 1]])
-    end
-  end
-  describe "top_common_word_sequences" do
-    hash = {}
-    100.times {|n| hash[n]=rand(101)}
-    let(:top) { MostCommonWordSequencesOfFile.new('blah.jpg').top_common_word_sequences( hash ) }
-    it "returns the top #{MostCommonWordSequencesOfFile::NUM_OF_SEQUENCES} values in the hash" do
-      expect(top.length).to eq(100)
     end
   end
 end
