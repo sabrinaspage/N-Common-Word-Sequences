67db890dc35582f5c3152f02363550a9d231dd7a
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2023-02-10 16:37:34 -0500

    add expected hash to other class

--- spec/most_common_word_sequences_of_file_spec.rb
+++ spec/most_common_word_sequences_of_file_spec.rb
@@ -18,8 +18,21 @@ describe MostCommonWordSequencesOfFile do
     context 'txt file has data' do
       let!(:file_name) { 'spec/full_of_.txt' }
       it 'returns a string' do
-        expect(subject).to be_a_kind_of(Hash)
         expect(subject.length).to be <= MostCommonWordSequencesOfFile::NUM_OF_SEQUENCES
+        expect(subject).to eq(
+          {
+            'being silenced time' => 1,
+            'first time being' => 1,
+            'is not the' => 1,
+            'not the onion’s' => 1,
+            'onion’s first time' => 1,
+            'sadly this is' => 1,
+            'silenced time being' => 1,
+            'the onion’s first' => 1,
+            'this is not' => 1,
+            'time being silenced' => 2
+          }
+        )
       end
     end
   end
