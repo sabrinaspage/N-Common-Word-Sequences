2ec0f0c123d4f178ea07f4c8b99b61e77cfc6d83
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2023-02-11 11:12:44 -0500

    view time in seconds

--- lib/run.rb
+++ lib/run.rb
@@ -2,7 +2,10 @@
 
 require './lib/main'
 
+time_start = Time.now
 Main.new(ARGV).call.each_with_index { |hash, index|
   print ARGV[index], "\n"
   pp hash
 }
+time_end = Time.now
+puts "Time: #{(time_end - time_start)} seconds"
